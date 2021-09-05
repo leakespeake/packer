9d740cf49a4af1732d4e10bf0dfcfb3d2261ea3a
leakespeake <barryleake@hotmail.com>
2021-09-05 11:47:10 +0100

    template rebuild

--- builders/vmware/vsphere-iso/ubuntu-server-20-04/hcl2/http/scripts/cleanup.sh
+++ builders/vmware/vsphere-iso/ubuntu-server-20-04/hcl2/http/scripts/cleanup.sh
@@ -14,4 +14,12 @@ sudo rm -rf /tmp/*
 echo 'BASH HISTORY CLEANUP'
 cat /dev/null > ~/.bash_history
 
+# vmware guest os customization with cloud-init fails on the network configuration for ubuntu 20.04.x live server and later - fixes below;
+echo 'FIX VMWARE GUEST OS CUSTOMIZATION WITH SUBIQUITY'
+sudo sed -i '/^\[Unit\]/a After=dbus.service' /lib/systemd/system/open-vm-tools.service
+sudo awk 'NR==11 {$0="#D /tmp 1777 root root -"} 1' /usr/lib/tmpfiles.d/tmp.conf | sudo tee /usr/lib/tmpfiles.d/tmp.conf
+
+echo 'DISABLE CLOUD INIT'
+sudo touch /etc/cloud/cloud-init.disabled
+
 exit
